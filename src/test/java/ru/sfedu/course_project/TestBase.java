aee6b173503b2b6df42857ea6e190f2bd533fb74
DenisGridin <denisgridin6@gmail.com>
2020-12-02 21:26:53 +0300

    Add test for create/get presentation methods

--- src/test/java/ru/sfedu/course_project/TestBase.java
+++ src/test/java/ru/sfedu/course_project/TestBase.java
@@ -1,1 +1,14 @@
+package ru.sfedu.course_project;
 
+import ru.sfedu.course_project.bean.Presentation;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class TestBase {
+    public Presentation makePresentation () {
+        HashMap arguments = new HashMap();
+        Presentation presentation = new Presentation(arguments);
+        return presentation;
+    }
+}
--- src/test/java/ru/sfedu/course_project/api/DataProviderCSVTest.java
+++ src/test/java/ru/sfedu/course_project/api/DataProviderCSVTest.java
@@ -1,7 +1,44 @@
 package ru.sfedu.course_project.api;
 
+import org.junit.jupiter.api.Test;
+import ru.sfedu.course_project.TestBase;
+import ru.sfedu.course_project.bean.Presentation;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class DataProviderCSVTest {
+class DataProviderCSVTest extends TestBase {
 
+    @Test
+    void createPresentationSuccess() throws IOException {
+        System.out.println("createPresentationSuccess");
+        DataProviderCSV provider = new DataProviderCSV();
+        HashMap args = new HashMap();
+        Presentation presentation = makePresentation();
+        provider.createPresentation(args);
+        String presId = String.valueOf(presentation.getId());
+        HashMap params = new HashMap();
+        params.put("id", presId);
+        assertEquals(presentation.toString(),
+                provider.getPresentationById(params).get().toString());
+    }
+
+    @Test
+    void createPresentationFail() throws IOException {
+        System.out.println("createPresentationFail");
+        DataProviderCSV provider = new DataProviderCSV();
+        HashMap args = new HashMap();
+        provider.createPresentation(args);
+        String presId = String.valueOf(UUID.randomUUID()); // set random id for fail check
+        HashMap params = new HashMap();
+        params.put("id", presId);
+        assertNull(provider.getPresentationById(params).orElse(null));
+    }
+
+    @Test
+    void getPresentationById() {
+    }
 }
